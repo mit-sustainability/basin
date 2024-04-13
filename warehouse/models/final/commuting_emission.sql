b73c725451bef4309112bb11d12890502738328f
Yu Cheng <yu_cheng@mit.edu>
2024-04-13 14:21:11 -0400

    fic

--- warehouse/models/final/commuting_emission.sql
+++ warehouse/models/final/commuting_emission.sql
@@ -3,19 +3,19 @@ WITH combined AS (
         s18."mode",
         s18.mtco2,
         2018 AS "year"
-    FROM staging.stg_commuting_survey_2018 AS s18
+    FROM {{ ref('stg_commuting_survey_2018') }} AS s18
     UNION ALL
     SELECT
         s21."mode",
         s21.mtco2,
         2021 AS "year"
-    FROM staging.stg_commuting_survey_2021 AS s21
+    FROM {{ ref('stg_commuting_survey_2021') }} AS s21
     UNION ALL
     SELECT
         s23."mode",
         s23.mtco2,
         2023 AS "year"
-    FROM staging.stg_commuting_survey_2023 AS s23
+    FROM {{ ref('stg_commuting_survey_2023') }} AS s23
 )
 
 SELECT * FROM combined
