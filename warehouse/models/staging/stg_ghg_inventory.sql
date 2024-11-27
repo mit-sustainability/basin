530e89a528477ad0dde736e68f57172a3c21ec47
Yu Cheng <yu_cheng@mit.edu>
2024-11-27 17:10:48 -0500

    fix mtco2 groupby error

--- warehouse/models/staging/stg_ghg_inventory.sql
+++ warehouse/models/staging/stg_ghg_inventory.sql
@@ -54,7 +54,7 @@ pgs AS (
     -- Aggregate purchased goods and services emissions by fiscal year
     SELECT
         '3.1 Purchased Goods and Services' AS category,
-        mtco2 AS emission,
+        SUM(mtco2) AS emission,
         fiscal_year,
         3 AS "scope",
         CURRENT_TIMESTAMP AS last_update
