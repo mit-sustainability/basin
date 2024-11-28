5ca976886a9b59c34175c268a231e49a067a278a
Yu Cheng <yu_cheng@mit.edu>
2024-11-28 00:02:48 -0500

    patch final purchased goods summary model

--- warehouse/models/final/purchased_goods_invoice_summary.sql
+++ warehouse/models/final/purchased_goods_invoice_summary.sql
@@ -5,7 +5,7 @@ SELECT
     sum(inflated_spend) AS spend_usd,
     max(level_2) AS level_2,
     max(level_1) AS level_1,
-    (sum(ghg) / sum(sum(ghg)) OVER (PARTITION BY fiscal_year)) * 100 AS share_mtco2,
+    (sum(mtco2) / sum(sum(mtco2)) OVER (PARTITION BY fiscal_year)) * 100 AS share_mtco2,
     (sum(inflated_spend) / sum(sum(inflated_spend)) OVER (PARTITION BY fiscal_year))
     * 100 AS share_spend,
     current_timestamp AS last_update
